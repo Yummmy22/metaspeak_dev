35c45fd1cf393b440e45b3a2acd16a5f3932280a
Yummmmy22 <dimas.japan.drummer@gmail.com>
2024-01-14 08:29:34 +0700

    disatuin

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public bool isPaused;
+
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
