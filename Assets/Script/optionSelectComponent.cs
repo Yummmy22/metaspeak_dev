ec11607b61e18918c90e74dd24371cf5e3423a17
greenstar <89572173+greenstarishere@users.noreply.github.com>
2024-01-16 22:31:24 +0700

    mantap bro

--- Assets/Script/optionSelectComponent.cs
+++ Assets/Script/optionSelectComponent.cs
@@ -22,6 +22,7 @@ public class optionSelectComponent : MonoBehaviour
 
     public void selectButton(int index)
     {
+        //ini gampangla, ambil child dari panel, ntar klik button sesuai argument indexnya #letopedcook
         UIConversationButton[] childs = panel.GetComponentsInChildren<UIConversationButton>();
         UIConversationButton targetButton = childs[index];
         targetButton.OnHover(true);
